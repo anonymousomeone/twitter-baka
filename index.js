cc0d371837b47c0cbe85a970a3492c20c9576115
anonymousomeone <leoistheworst22@gmail.com>
2022-08-27 15:24:31 -0700

    twitter api

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { Client } = require('discord.js');
-const { token } = require('./token.json');
+const { TwitterApi } = require('twitter-api-v2');
+const token = require('./token.json');
+
 const client = new Client;
+const twitter = new TwitterApi({
+  appKey: token.apikey,
+  appSecret: token.apikeysecret,
+  accessToken: token.accesstoken,
+  accessSecret: token.accesstokensecret,
+  bearer: token.bearer
+}).v2;
+
+console.log(twitter.me())
+twitter.tweet('hello world!')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,4 +24,4 @@ client.on('message', msg => {
   }
 });
 
-client.login(token);
+client.login(token.token);
